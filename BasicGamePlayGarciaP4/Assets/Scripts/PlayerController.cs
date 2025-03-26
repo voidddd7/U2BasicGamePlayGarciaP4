f66f217b7fcb354c579292eff9867ffb7404d3c4
voidddd7 <726888@student.dusd.net>
2025-03-26 11:41:20 -0700

    finished scripts
    finished 2 scripts with spawning and projectile

--- BasicGamePlayGarciaP4/Assets/Scripts/PlayerController.cs
+++ BasicGamePlayGarciaP4/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
     void Update()
     {
         if(transform.position.x < -xRange)
@@ -26,5 +28,11 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
